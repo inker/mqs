d107cc8e7ed1e3ade52523f809d475f990c11736
Anton <anton.veselyev@gmail.com>
2017-11-16 22:42:47 +0300

    comments

--- src/utils/transferable/fromBuffer.js
+++ src/utils/transferable/fromBuffer.js
@@ -1,5 +1,10 @@
 const decoder = new TextDecoder('utf-8')
 
+/**
+ * ArrayBuffer -> object
+ * @param {ArrayBuffer} buffer
+ * @returns {object}
+ */
 export default (buffer) => {
   const str = decoder.decode(buffer)
   return JSON.parse(str)
--- src/utils/transferable/toBuffer.js
+++ src/utils/transferable/toBuffer.js
@@ -1,5 +1,10 @@
 const encoder = new TextEncoder('utf-8')
 
+/**
+ * object -> ArrayBuffer
+ * @param {object} o
+ * @returns {ArrayBuffer}
+ */
 export default (o) => {
   const str = JSON.stringify(o)
   const arr = encoder.encode(str)
